64068da3aae273f5a67dc94e7c0721ea7079f7fa
aalina888 <alinaarap@gmail.com>
2020-04-10 17:11:01 +0300

    Queue Comments Added

--- src/main/java/DataStructures/Queue/Queue.java
+++ src/main/java/DataStructures/Queue/Queue.java
@@ -32,29 +32,38 @@ public class Queue {
     }
 
     public void insert(City city) {
+        // Insert a new element at the end of the Queue
         if (head == null) {
             head = new Node(city);
         } else {
             Node last = head;
 
+            // Go to the last element of the Queue
             while (last.getNext() != null) {
                 last = last.getNext();
             }
 
+            // Create new element
+            // It would be next for the last
             last.setNext(new Node(city));
         }
 
-        // Increment Size of the List
+        // Increment Size of the Queue
         size++;
     }
 
     public Node search(String city) {
+        // If Queue is empty return null
         if (head == null) {
             return null;
         } else {
             Node iterator = head;
 
+            // Searching for the element
             while(iterator != null) {
+
+                // If the element was found
+                // Return it
                 if (iterator.getData().getName().equals(city)) {
                     return iterator;
                 }
@@ -67,11 +76,14 @@ public class Queue {
     }
 
     public void delete() {
+        // Delete first element from the Queue
+        // If Queue is not empty
+        // Assign head to next
         if (head != null) {
             head = head.getNext();
         }
 
-        // Decrement Size of the List
+        // Decrement Size of the Queue
         size--;
     }
 }
