88d80d0b54c46b2fb9c54c3a321cb6534a85661c
aalina888 <alinaarap@gmail.com>
2020-04-11 17:37:40 +0300

    Insert in Binary Tree Comments Added

--- src/main/java/DataStructures/BinaryTree/BinaryTree.java
+++ src/main/java/DataStructures/BinaryTree/BinaryTree.java
@@ -11,23 +11,42 @@ public class BinaryTree {
     }
 
     public void insert(City city) {
+        // If tree is empty
         if (head == null) {
+
+            // Create head
             head = new Node(city);
             size++;
         } else {
+
+            // Examine all elements from the tree
             while (head != null) {
+
+                // If city to insert is less
                 if (city.compareTo(head.getData()) < 0) {
+
+                    // And there is less element
                     if (head.getLess() != null) {
+
+                        // Go to the element
                         head = head.getLess();
                     } else {
+
+                        // Insert new element
                         head.setLess(new Node(city));
                         size++;
                         return;
                     }
                 } else {
+
+                    // If there is more element
                     if (head.getMore() != null) {
+
+                        // Go to the element
                         head = head.getMore();
                     } else {
+
+                        // Insert new element
                         head.setMore(new Node(city));
                         size++;
                         return;
